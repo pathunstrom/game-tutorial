5ab4e10bb2c9c6b00f7ecfc8cfdbb6a8b51f2441
Piper Thunstrom <pathunstrom@gmail.com>
2018-09-07 23:57:28 -0400

    Shooter 4-main.py rewritten to match up to Commit c67239c of ppb

--- shooter-tutorial/sample/4-main.py
+++ shooter-tutorial/sample/4-main.py
@@ -1,41 +1,23 @@
-from os import path
-
-from ppb import BaseScene, GameEngine
-from pygame import image
-from pygame import mouse
-from pygame.sprite import DirtySprite
+import ppb
+from ppb import BaseSprite
+from ppb.events import Update
 
 
-class Game(BaseScene):
-    def __init__(self, engine, background_color=(0, 0, 0), **kwargs):
-        super().__init__(engine=engine,
-                         background_color=background_color,
-                         **kwargs)
-        Player(self)
-
-
-class Player(DirtySprite):
-
-    def __init__(self, scene):
-        super().__init__(scene.groups["player"])
-        self.image = image.load(path.join(path.dirname(__file__),
-                                          "player.png"))
-        self.rect = self.image.get_rect()
-        self.scene = scene
-
-    def update(self, time_delta):
-        mouse_x, mouse_y = mouse.get_pos()
-        diff_x = max(min(mouse_x - self.rect.centerx, 5), -5)
-        diff_y = max(min(mouse_y - self.rect.centery, 5), -5)
-        self.rect.centerx += diff_x
-        self.rect.centery += diff_y
-        if diff_x or diff_y:
-            self.dirty = True
+class Player(BaseSprite):
+    def on_update(self, update: Update, signal):
+        self.position += (update.mouse.position - self.position).scale(3) * update.time_delta
 
 
 def main():
-    with GameEngine(Game, resolution=(400, 600)) as engine:
-        engine.run()
+    game_kwargs = {"set_up": scene_setup}
+
+    ppb.run(resolution=(800, 800),
+            scene_kwargs=game_kwargs)
+
+
+def scene_setup(scene):
+    scene.add(Player())
+
 
 if __name__ == "__main__":
     main()
